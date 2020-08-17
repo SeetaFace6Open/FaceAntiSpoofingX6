503c2c73c2377a7ae8a94d9560332d07ec3c5a33
bo.zhang <bo.zhang@seetatech.com>
2020-08-17 17:17:03 +0800

    fix shell

--- craft/build.linux.x64.sh
+++ craft/build.linux.x64.sh
@@ -6,7 +6,7 @@ export PLATFORM_TARGET=x64
 
 export PLATFORM=x64
 export ORZ_HOME=$(cd "$(dirname "$0")"; pwd)/../../build
-export INSTALL_DIR=$(cd "$(dirname "$0")"; pwd) /../../build
+export INSTALL_DIR=$(cd "$(dirname "$0")"; pwd)/../../build
 
 HOME=$(cd `dirname $0`; pwd)
 
